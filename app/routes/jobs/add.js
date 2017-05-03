06e8f3536b4c03f25dc6bba1ea7eea97e583dfd3
kecorbin <kecorbin@cisco.com>
2017-05-02 21:02:19 -0400

    add route/template/logic for adding jobs

--- app/routes/jobs/add.js
+++ app/routes/jobs/add.js
@@ -2,11 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   actions: {
-    createJob(job) {
-      console.log(this)
-      console.log("adding job for " + this.controller.newName )
-      console.log("using image " + this.controller.image)
-      //
+    createJob() {
+      // called when submit button is pushed
+
+      // gather info
+      let controller = this.get('controller');
+
+      let job = this.store.createRecord('job', {
+        name: controller.get("name"),
+        image: controller.get("image"),
+        interval: {every: controller.get("every"),
+                   period: controller.get("period")},
+        queue: controller.get("queue"),
+
+      });
+      job.save()
+
+      // redirect user back to jobs page
       //this.transitionTo('jobs')
     }
   }
