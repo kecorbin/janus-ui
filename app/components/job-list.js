c7c0b06c3fbdea2410a0a4aaec10499c1fa3d9d3
kecorbin <kecorbin@cisco.com>
2017-05-02 22:28:44 -0400

    remove deleteJob from job-list component

--- app/components/job-list.js
+++ app/components/job-list.js
@@ -1,10 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
-        deleteJob: function() {
-            console.log(this);
-            this.get('job').destroyRecord();
-        }
-    }
 });
