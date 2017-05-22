c137e49b950ffecfc0558777dfb71423aef87b16
kecorbin <kecorbin@cisco.com>
2017-05-22 15:16:40 -0500

    only display url if one exists

--- app/serializers/tunnel.js
+++ app/serializers/tunnel.js
@@ -1,6 +1,25 @@
 import DRFSerializer from './drf';
 
 export default DRFSerializer.extend({
+  normalizeResponse(store, primaryModelClass, payload) {
+    if (payload instanceof Array) {
+        payload.forEach(parseResults)
+    }
+    else {
+      parseResults(payload)
+    }
+
+    // if url contains None, we null it out
+    function parseResults(tunnel) {
+      if (tunnel.url.includes('None')) {
+          tunnel.url = null
+      }
+
+      return tunnel
+    }
+    return this._super(...arguments);
+  },
+
   attrs: {
     sourceip: { serialize: false },
   }
