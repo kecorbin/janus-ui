6ecd455db2e9f567b19409272c8ad9514c4d9adf
kecorbin <kecorbin@cisco.com>
2017-05-23 17:31:06 -0500

    ui improvements for speedtests

--- app/models/speedtest.js
+++ app/models/speedtest.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   jobId: DS.belongsTo('job'),
   results: DS.attr(),
   processed: DS.attr('boolean'),
-  type: DS.attr()
+  type: DS.attr(),
+  created: DS.attr('date')
 });
--- app/routes/speedtests/index.js
+++ app/routes/speedtests/index.js
@@ -1,9 +1,38 @@
 import Ember from 'ember';
 
+
 export default Ember.Route.extend({
+
+  generatedata() {
+    var dataMap = [['Date', 'Download', 'Upload']]
+    var store = this.get('store').findAll('speedtest').then(
+      function(results) {
+        results.forEach(function(r) {
+          var resultMap = [
+            r.get('created'),
+            parseFloat(r.get('results').download),
+            parseFloat(r.get('results').upload)
+          ]
+          console.log(resultMap)
+          dataMap.push(resultMap)
+        }
+        )
+      }
+    )
+    return dataMap
+  },
   model() {
     return {
-      'speedtests': this.get('store').findAll('speedtest', { reload: true })
+      'speedtests': this.get('store').findAll('speedtest', { reload: true }),
+      'chartdata': this.generatedata()
+
+    //   [
+    //   ['Date', 'Download', 'Upload'],
+    //   ['2004', 1000, 400],
+    //   ['2005', 1170, 460],
+    //   ['2006', 660, 1120],
+    //   ['2007', 1030, 540],
+    // ],
     };
   }
 });
