83f96680c11ff82d0abf5b160edfac0aec0599f8
kecorbin <kecorbin@cisco.com>
2017-05-28 01:04:09 -0500

    add dropdown select for scan type

---
+++ app/controllers/scans/add.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  type: null,
+  types: Ember.String.w('quick-vendor scan'),
+  actions: {
+    selectType(type) {
+      this.set('type', type);
+      console.log(type)
+    }
+  }
+});
