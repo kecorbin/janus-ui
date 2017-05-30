75175a24b9886cccb9f273f4ea2958b364076a35
kecorbin <kecorbin@cisco.com>
2017-05-30 00:54:14 -0500

    update adapters for jobs/results relationship

--- app/adapters/job.js
+++ app/adapters/job.js
@@ -2,6 +2,15 @@ import ApplicationAdapter from './application';
 import ENV from 'collins-ui/config/environment';
 
 export default ApplicationAdapter.extend({
+  // override trailing slashes when we have nested
   host: ENV.apiHost,
   namespace: 'api/v1/act',
+
+  addTrailingSlashes: false,
+  urlForFindAll(model, snapshot) {
+    return ENV.apiHost + '/api/v1/act/jobs/'
+  },
+  urlForCreateRecord(model, snapshot) {
+    return ENV.apiHost + '/api/v1/act/jobs/'
+  }
 });
--- app/adapters/result.js
+++ app/adapters/result.js
@@ -3,5 +3,5 @@ import ENV from 'collins-ui/config/environment';
 
 export default ApplicationAdapter.extend({
   host: ENV.apiHost,
-  namespace: 'api/v1/act',
+  // namespace: 'api/v1/act',
 });
