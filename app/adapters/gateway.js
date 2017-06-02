28f5f7baac4f0c39e1cef3d2e22b41f5ffcb2a65
kecorbin <kecorbin@cisco.com>
2017-06-02 16:04:27 -0500

    quick scan fix

--- app/adapters/gateway.js
+++ app/adapters/gateway.js
@@ -1,4 +1,5 @@
 import ApplicationAdapter from './application';
+import ENV from 'collins-ui/config/environment';
 
 export default ApplicationAdapter.extend({
   host: ENV.apiHost,
--- app/controllers/scans/add.js
+++ app/controllers/scans/add.js
@@ -6,6 +6,18 @@ export default Ember.Controller.extend({
   actions: {
     selectType(type) {
       this.set('type', type)
+    },
+    createScan() {
+      // called when submit button is pushed
+      console.log(this.get('type'))
+      // gather info
+      let scan = this.store.createRecord('scan', {
+        destination: this.get("destination"),
+        type: this.type,
+      });
+      scan.save().then(() => this.transitionTo('scans'));
+
     }
   }
+
 });
